f04268bcd06325c36fee2d7fe7479d93a35ea8e7
carreterosanti <unicen123>
2019-11-30 20:12:31 -0300

    Updates

---
+++ precache-manifest.82b5a7aa8bb1a5117f86a96d128079fa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fc561346551aeac16c97ddedbc9dd4",
+    "url": "/personal-resume/index.html"
+  },
+  {
+    "revision": "388fc01c8387e32f15da",
+    "url": "/personal-resume/static/css/main.35a6ab6d.chunk.css"
+  },
+  {
+    "revision": "285615a16a3056fe1caa",
+    "url": "/personal-resume/static/js/2.b73c969b.chunk.js"
+  },
+  {
+    "revision": "388fc01c8387e32f15da",
+    "url": "/personal-resume/static/js/main.a7a9eccd.chunk.js"
+  },
+  {
+    "revision": "a2841f6cfb1e5d3b7903",
+    "url": "/personal-resume/static/js/runtime-main.c6db0b59.js"
+  },
+  {
+    "revision": "c1323353e37944147f5123f794261180",
+    "url": "/personal-resume/static/media/java-fx-icon.c1323353.png"
+  },
+  {
+    "revision": "82f4b91efc6d84f72579733b4e046801",
+    "url": "/personal-resume/static/media/js-icon.82f4b91e.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/personal-resume/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/personal-resume/static/media/react-icon.47ce6e77.png"
+  },
+  {
+    "revision": "b75bd53376d5768a4971df5f4d35a80a",
+    "url": "/personal-resume/static/media/reciclados-tandil-app.b75bd533.png"
+  },
+  {
+    "revision": "3f72acd2aa6beaf5785ab0196de85a19",
+    "url": "/personal-resume/static/media/redesign-web-bac-mono.3f72acd2.png"
+  },
+  {
+    "revision": "7cb20968686d6115c18510784a279368",
+    "url": "/personal-resume/static/media/reemplazo-de-smartphones.7cb20968.png"
+  },
+  {
+    "revision": "34cebfd4b532e89e8c3b13ffcad7963e",
+    "url": "/personal-resume/static/media/vsco-icon.34cebfd4.png"
+  }
+]);
