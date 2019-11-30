345897475335385a256f5947e4ca068cd0282cec
carreterosanti <unicen123>
2019-11-30 19:17:47 -0300

    Hobbies & Intereses v1.0

--- src/components/Intereses.js
+++ src/components/Intereses.js
@@ -3,26 +3,48 @@ import React from "react";
 export default function Intereses(props) {
   const listadoIntereses = [
     {
-      srcInteresImg: "css-icon.png"
-    },
-    {
-      srcInteresImg: "js-icon.png"
-    },
-    {
-      srcInteresImg: "react-icon.png"
+      srcInteresImg: props.darkMode
+        ? "dark-mode-crossfit-icon.png"
+        : "crossfit-icon.png",
+      label: "Crossfit"
     },
     {
       srcInteresImg: props.darkMode
-        ? "dark-mode-github-icon.png"
-        : "github-icon.png"
+        ? "dark-mode-design-icon.png"
+        : "design-icon.png",
+      label: "Diseño"
+    },
+    {
+      srcInteresImg: props.darkMode ? "dark-mode-car-icon.png" : "car-icon.png",
+      label: "Autos"
+    },
+    {
+      srcInteresImg: props.darkMode ? "dark-mode-ski-icon.png" : "ski-icon.png",
+      label: "Esquí"
+    },
+    {
+      srcInteresImg: props.darkMode
+        ? "dark-mode-coffee-icon.png"
+        : "coffee-icon.png",
+      label: "Café"
     }
   ];
 
   const iconosIntereses = listadoIntereses.map(element => (
-    <img
-      className="interes-icon"
-      src={require("./images/" + element.srcInteresImg)}
-    />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+      <img
+        className="interes-icon"
+        src={require("./images/" + element.srcInteresImg)}
+      />
+      <h4 style={{ margin: "1px 0px", fontWeight: "450" }}>{element.label}</h4>
+    </div>
   ));
 
   return (
