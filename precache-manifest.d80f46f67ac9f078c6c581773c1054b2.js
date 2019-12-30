d339831ebf7bf546f7c46db95c2ea45e91b0d8ee
carreterosanti <unicen123>
2019-12-29 23:12:10 -0300

    Updates

--- precache-manifest.d80f46f67ac9f078c6c581773c1054b2.js
+++ precache-manifest.d80f46f67ac9f078c6c581773c1054b2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16d873bb3d282e1d41bc1cefb5c607ac",
+    "revision": "7f3956c78c862f6b28d6982fc4e034ff",
     "url": "/Personal-Resume/index.html"
   },
   {
-    "revision": "1d8f3efc2a29ae91dc21",
-    "url": "/Personal-Resume/static/css/main.27c3a3b6.chunk.css"
+    "revision": "f30767d2739d9b4d1593",
+    "url": "/Personal-Resume/static/css/main.62cec86a.chunk.css"
   },
   {
     "revision": "e291c8581b385f2399e9",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Personal-Resume/static/js/2.c19136f7.chunk.js.LICENSE"
   },
   {
-    "revision": "1d8f3efc2a29ae91dc21",
-    "url": "/Personal-Resume/static/js/main.9ddf7a99.chunk.js"
+    "revision": "f30767d2739d9b4d1593",
+    "url": "/Personal-Resume/static/js/main.af6e3260.chunk.js"
   },
   {
     "revision": "0bf8913e1f370f5cfd13",
@@ -38,10 +38,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "82f4b91efc6d84f72579733b4e046801",
     "url": "/Personal-Resume/static/media/js-icon.82f4b91e.png"
-  },
-  {
-    "revision": "25bf045ca257e971124f3997d89f321c",
-    "url": "/Personal-Resume/static/media/logo.25bf045c.svg"
   },
   {
     "revision": "47ce6e77f039020ee2e76a10c1e988e9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Personal-Resume/precache-manifest.e2e5a0148a7f24f1d1e43a80f28bf9cd.js"
+  "/Personal-Resume/precache-manifest.d80f46f67ac9f078c6c581773c1054b2.js"
 );
 
 self.addEventListener('message', (event) => {
