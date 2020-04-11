df3c9ebf45fb6665b427e1c4e414d7ee23b97a36
carreterosanti <unicen123>
2020-04-11 19:57:49 -0300

    Adición de Curso The React Bootcamp en curso

--- src/components/Cursos.js
+++ src/components/Cursos.js
@@ -7,6 +7,12 @@ export default function Cursos(props) {
       <h2>Cursos</h2>
       <div className="contenido-section">
         <CursosItem
+          titulo="The React Bootcamp - Become a professional React developer"
+          plataforma="Scrimba"
+          darkMode={props.darkMode}
+          linkref="https://scrimba.com/g/greact"
+        />
+        <CursosItem
           titulo="Learn React - A comprehensive introduction"
           plataforma="Scrimba"
           darkMode={props.darkMode}
