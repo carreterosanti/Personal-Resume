17531609e3d3d9f57546e00a4648f1571cecb8b7
carreterosanti <santiago.carretero@outlook.com>
2020-04-19 12:38:18 -0300

    Cambio fecha de nacimiento, por edad

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,32 +2,44 @@ import React from "react";
 import "./components-style.css";
 
 export default function NavBar(props) {
+  const fechaNacimientoSantiagoCarretero = new Date(1995, 12, 26);
+  function getEdad(fechaNacimiento) {
+    var diferenciaMilisegundos = Date.now() - fechaNacimiento.getTime();
+    var edad = new Date(diferenciaMilisegundos);
+    return Math.abs(edad.getUTCFullYear() - 1970);
+  }
+
   return (
     <div className="main-nav-bar-div">
+      {console.log()}
       <div className="main-name-div">
         <h1 className="main-name">Santiago Carretero</h1>
-        <div style={{ display: "flex" }}></div>
-        <p>
-          Mar del Plata, Argentina - 26/12/1995 -{" "}
-          <a
-            href="mailto:santiago.carretero@outlook.com"
-            className="clickable-text"
-            style={
-              props.darkMode
-                ? { fontSize: "16px", color: "#fafafa" }
-                : { fontSize: "16px", color: "#333" }
-            }
-          >
-            santiago.carretero@outlook.com
-          </a>
-        </p>
+        <div className="contenido-section" style={{ display: "flex" }}>
+          <p>
+            Mar del Plata, Argentina -{" "}
+            {getEdad(fechaNacimientoSantiagoCarretero)} años -{" "}
+            <a
+              href="mailto:santiago.carretero@outlook.com"
+              className="clickable-text"
+              style={
+                props.darkMode
+                  ? { fontSize: "16px", color: "#fafafa" }
+                  : { fontSize: "16px", color: "#333" }
+              }
+            >
+              santiago.carretero@outlook.com
+            </a>
+          </p>
+        </div>
       </div>
-      <img
-        className="profile-image swing"
-        src={require("./images/profile-picture.jpg")}
-        alt="Profile picture."
-        onClick={props.setDarkMode}
-      />
+      <div style={{ position: "relative" }}>
+        <img
+          className="profile-image swing"
+          src={require("./images/profile-picture.jpg")}
+          alt="Profile picture."
+          onClick={props.setDarkMode}
+        />
+      </div>
     </div>
   );
 }
