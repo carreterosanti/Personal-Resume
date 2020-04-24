7dca87b28b76980ca5f1926d6532ada17c0e4b2b
carreterosanti <santiago.carretero@outlook.com>
2020-04-24 00:16:22 -0300

    Reducción margen navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,15 +13,17 @@ export default function NavBar(props) {
     <div className="main-nav-bar-div">
       <div className="main-name-div">
         <h1 className="main-name">Santiago Carretero</h1>
-        <p>Mar del Plata, Argentina</p>
-        <p>{getEdad(fechaNacimientoSantiagoCarretero)} años</p>
+        <p style={{ margin: "10px 0px" }}>Mar del Plata, Argentina</p>
+        <p style={{ margin: "10px 0px" }}>
+          {getEdad(fechaNacimientoSantiagoCarretero)} años
+        </p>
         <a
           href="mailto:santiago.carretero@outlook.com"
           className="clickable-text"
           style={
             props.darkMode
-              ? { fontSize: "16px", color: "#fafafa" }
-              : { fontSize: "16px", color: "#333" }
+              ? { fontSize: "16px", color: "#fafafa", margin: "10px 0px" }
+              : { fontSize: "16px", color: "#333", margin: "10px 0px" }
           }
         >
           santiago.carretero@outlook.com
